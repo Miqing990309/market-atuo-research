2f5ba04b42b408ff92e99f097827b54227d4a9e1
KamenRiderCyan <57658982+KamenRiderCyan@users.noreply.github.com>
2023-07-27 00:58:25 +0800

    update sort func

--- src/sort/sort.py
+++ src/sort/sort.py
@@ -1,6 +1,28 @@
 import os
+# import xlsxwriter module
+import xlsxwriter
 
 directory = "output"
+
+def createSheet(name):
+    # Workbook() takes one, non-optional, argument
+    # which is the filename that we want to create.
+    workbook = xlsxwriter.Workbook(name)
+
+    # The workbook object is then used to add new
+    # worksheet via the add_worksheet() method.
+    worksheet = workbook.add_worksheet()
+
+    # Use the worksheet object to write
+    # data via the write() method.
+    worksheet.write('A1', 'Hello..')
+    worksheet.write('B1', 'Geeks')
+    worksheet.write('C1', 'For')
+    worksheet.write('D1', 'Geeks')
+
+    # Finally, close the Excel file
+    # via the close() method.
+    workbook.close()
 
 def main():
     # current dir
@@ -10,6 +32,8 @@ def main():
     path = os.path.join(current_path, directory)
     if not os.path.exists(path):
         os.mkdir(path)
+
+
 
 if __name__ == '__main__':
     main()
